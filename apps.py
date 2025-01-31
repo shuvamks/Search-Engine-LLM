01a7342415ab929a2aeae2330e47d86bfd05e320
Shubham Kumar <80683494+shuvamks@users.noreply.github.com>
2025-01-31 13:15:16 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,65 @@
+## TOOLS and AGENTS
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import (
+    ArxivQueryRun,
+    WikipediaQueryRun,
+    DuckDuckGoSearchRun,
+)
+from langchain_community.utilities import WikipediaAPIWrapper, ArxivAPIWrapper
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+import os
+from dotenv import load_dotenv
+
+
+## Arxiv and Wikipedia Tools
+api_wrapper_wiki = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=250)
+wiki = WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
+api_wrapper_arxiv = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=250)
+arxiv = ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
+search = DuckDuckGoSearchRun(name="Search ")
+
+
+st.title("🔎 LangChain - Chat with search")
+"""
+In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
+"""
+
+
+st.sidebar.title("Settings")
+api_key = st.sidebar.text_input("Enter your Open AI API Key:", type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {
+            "role": "assistant",
+            "content": "Hi, I'm a chatbot who can search the web. How can I help you?",
+        }
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
+if prompt := st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    llm = ChatOpenAI(openai_api_key=api_key, model_name="gpt-4-turbo", streaming=True)
+    tools = [search, arxiv, wiki]
+
+    search_agent = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        handling_parsing_errors=True,
+    )
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
