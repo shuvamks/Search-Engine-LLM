0b1bccf8c3a144aed7d074d57f8284250caae91e
Shubham Kumar <80683494+shuvamks@users.noreply.github.com>
2025-01-31 15:54:33 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -12,7 +12,7 @@ from langchain.agents import initialize_agent, AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
-
+##code
 
 ## Arxiv and Wikipedia Tools
 api_wrapper_wiki = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=250)
